7baf45615ecf9a7dd9210aca1ccc638869af187d
David Battersby <info@davidbattersby.com>
2020-03-06 20:45:16 +0800

    add posts scaffold, style homepage, add extra pages

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,16 @@
 class PublicController < ApplicationController
 
   def homepage
+    @posts = Post.all
+  end
+
+  def about
+  end
+
+  def blog
+  end
+
+  def contact
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "about" => "public#about", as: :about
+  get "blog" => "public#blog", as: :blog
+  get "contact" => "public#contact", as: :contact
 
   root to: "public#homepage"
 end
